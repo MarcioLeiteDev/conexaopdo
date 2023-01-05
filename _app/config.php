03ff9a9cd06461df0ef22332a15b44b41f7d0570
MarcioLeiteDev <contato@marcioleitedev.com.br>
2023-01-05 16:47:17 -0300

    first commit

---
+++ _app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$dbUser = "root";
+$dbPass = "pass";
+$dbName = "name_bd";
+
+define("CONF_DB_HOST", $host);
+define("CONF_DB_USER", $dbUser);
+define("CONF_DB_PASS", $dbPass);
+define("CONF_DB_NAME", $dbName);
+
